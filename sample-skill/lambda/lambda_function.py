f2678f361cc3ed7f559f73468a9cf33cb9f1a56b
Bryann Valderrama <bryanttandrei2015@gmail.com>
2023-09-18 16:42:01 -0500

    feat: add get_course_id function and update workflow of handler

--- sample-skill/lambda/lambda_function.py
+++ sample-skill/lambda/lambda_function.py
@@ -5,6 +5,8 @@
 # Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 # session persistence, api calls, and more.
 # This sample is built using the handler classes approach in skill builder.
+from __future__ import annotations
+
 import logging
 from http import HTTPStatus
 
@@ -42,15 +44,6 @@ class LaunchRequestHandler(AbstractRequestHandler):
         )
 
 
-COURSES = {
-    "h5p": "course-v1:eduNEXT+H5P101+2023_T2",
-    "mindmap": "course-v1:eduNEXT+MM101+2023_T2",
-    "demo": "course-v1:edX+DemoX+Demo_Course",
-    "limesurvey": "course-v1:edunext+lime-demo-1+2023",
-    "ora": "course-v1:eduNEXT+ORA101+2023_T2",
-    "openedx": "course-v1:eduNEXT+LS101+2013_T1",
-}
-
 # Settings
 ENDPOINT = "https://lms.valencia.athena.edunext.link"
 CLIENT_ID = "client_id"
@@ -59,10 +52,13 @@ GRANT_TYPE = "client_credentials"
 MAX_TIMEOUT = 5
 
 
-def get_bearer_token() -> tuple:
-    """Return the bearer token to consume the API"""
-    error_message, access_token = None, None
+def get_bearer_token() -> str | None:
+    """Return the bearer token to consume the API
 
+    Returns:
+        str: Bearer token
+        None: If the token can't be retrieved
+    """
     url = f"{ENDPOINT}/oauth2/access_token"
     payload = f'client_id={CLIENT_ID}&client_secret={CLIENT_SECRET}&grant_type={GRANT_TYPE}'
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
@@ -70,34 +66,87 @@ def get_bearer_token() -> tuple:
     response = requests.post(url, data=payload, headers=headers, timeout=MAX_TIMEOUT)
 
     if response.status_code == HTTPStatus.OK:
-        access_token = response.json()["access_token"]
-    else:
-        error_message =  "No fue posible consultar el progreso por un error de acceso."
+        return response.json()["access_token"]
 
-    return access_token, error_message
+    return None
 
 
-def get_course_progress(username: str, coursename: str) -> str:
-    """Return the progress of an user in a course"""
-    token, error_message = get_bearer_token()
+def get_course_progress(username: str, course_id: str, token: str) -> float | None:
+    """Return the progress of an user in a course
 
-    if not token:
-        return error_message
+    Args:
+        username (str): Username of the student
+        course_id (str): Course id
+        token (str): Bearer token to consume the API
 
+    Returns:
+        float: Progress of the student in the course
+        None: If the progress can't be retrieved
+    """
     url = f"{ENDPOINT}/eox-core/api/v1/grade/"
-    payload = {"username": username, "course_id": COURSES.get(coursename)}
+    payload = {"username": username, "course_id": course_id}
     headers = {"Authorization": f"Bearer {token}"}
 
     response = requests.get(url, data=payload, headers=headers, timeout=MAX_TIMEOUT)
 
     if response.status_code == HTTPStatus.OK:
-        course_progress = response.json()["earned_grade"]
-        return (
-            f"El progreso para el estudiante con nombre de usuario {username} "
-            f"en el curso de {coursename} es {round(course_progress*100, 2)}%."
-        )
-    else:
-        return "El usuario o el curso no se encuentra. Intente con uno válido."
+        return round(response.json()["earned_grade"]*100, 2)
+
+    return None
+
+
+def get_course_id(course_name: str) -> str | None:
+    """Return the course id by the course name
+
+    Args:
+        course_name (str): Course name
+
+    Returns:
+        str: Course id
+        None: If the course can't be retrieved
+    """
+    COURSES = {
+        "h5p": "course-v1:eduNEXT+H5P101+2023_T2",
+        "mindmap": "course-v1:eduNEXT+MM101+2023_T2",
+        "demo": "course-v1:edX+DemoX+Demo_Course",
+        "limesurvey": "course-v1:edunext+lime-demo-1+2023",
+        "ora": "course-v1:eduNEXT+ORA101+2023_T2",
+        "openedx": "course-v1:eduNEXT+LS101+2013_T1",
+    }
+
+    return COURSES.get(course_name)
+
+
+def get_speak_output_get_course_progress(handler_input: HandlerInput) -> str:
+    """Return the speak output for the Get Course Progress Intent
+
+    Returns:
+        str: Speak output
+    """
+    slots = handler_input.request_envelope.request.intent.slots
+
+    username = slots["username"].value.lower()
+    coursename = slots["coursename"].value.lower()
+
+    token = get_bearer_token()
+
+    if not token:
+        return "No fue posible consultar el progreso por un error de acceso."
+
+    course_id = get_course_id(coursename)
+
+    if not course_id:
+        return "El curso no se encuentra. Intente con uno válido."
+
+    course_progress = get_course_progress(username, course_id, token)
+
+    if not course_progress:
+        return "El usuario no se encuentra. Intente con uno válido."
+
+    return (
+        f"El progreso para el estudiante con nombre de usuario {username} "
+        f"en el curso de {coursename} es {course_progress}%."
+    )
 
 
 class GetCourseProgressIntentHandler(AbstractRequestHandler):
@@ -107,12 +156,8 @@ class GetCourseProgressIntentHandler(AbstractRequestHandler):
         return ask_utils.is_intent_name("GetCourseProgressIntent")(handler_input)
 
     def handle(self, handler_input: HandlerInput) -> Response:
-        slots = handler_input.request_envelope.request.intent.slots
 
-        coursename = slots["coursename"].value.lower()
-        username = slots["username"].value.lower()
-
-        speak_output = get_course_progress(username, coursename)
+        speak_output = get_speak_output_get_course_progress(handler_input)
 
         return (
             handler_input.response_builder
